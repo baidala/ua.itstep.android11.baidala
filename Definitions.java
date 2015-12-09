049737f9fca264bc8e40ea562bca6b540c7a60bb
baidala <stavr@bk.ru>
2015-12-09 11:25:57 +0200

    Create Definitions.java

---
+++ Definitions.java
@@ -0,0 +1,9 @@
+package inputoutput;
+
+public class Definitions {
+	final public static String FILE_DEFAULT_NAME = "d:\\workspace\\newfile.txt";
+    final public static int READ_CONSOLE_CASE = 1;
+    final public static int READ_FILE_CASE = 2;
+    final public static int COPY_FILE_CASE = 3;
+    final public static int READ_HTTP_CASE = 4;
+}
